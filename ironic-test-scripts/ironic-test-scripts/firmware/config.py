d44cf65e9464d3b05b96b5a6a3f968923d951041
openstackdev86 <openstackdev86@gmail.com>
2016-03-22 11:54:42 +0530

    delete old ironic test scripts

--- ironic-test-scripts/ironic-test-scripts/firmware/config.py
+++
@@ -1,7 +0,0 @@
-"""
-Handler information
-"""
-IP='172.16.247.158'
-USERNAME='ucspe'
-PASSWORD='ucspe'
-
--- ironic-test-scripts/ironic-test-scripts/network/config.py
+++
@@ -1,9 +0,0 @@
-"""
-Handler information
-"""
-IP='172.16.247.158'
-USERNAME='ucspe'
-PASSWORD='ucspe'
-
-
-DN='org-root/org-Finance'
